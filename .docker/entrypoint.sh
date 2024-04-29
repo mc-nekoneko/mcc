98c07d6abe93c5a4a5ab64a3922479acf6447bb6
Nekoneko <nekonekogame@gmail.com>
2024-04-29 19:30:09 +0900

    起動時のコマンドを表示するようにした

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,4 +3,7 @@ cd /home/minecraft
 
 java -version
 
-eval java -Xms${SERVER_MEMORY} -Xmx${SERVER_MEMORY} ${JVM_FLAGS} -jar ${SERVER_JARFILE} ${SERVER_ARGS}
+STARTUP_COMMAND="java -Xms${SERVER_MEMORY} -Xmx${SERVER_MEMORY} ${JVM_FLAGS} -jar ${SERVER_JARFILE} ${SERVER_ARGS}"
+
+echo "Running command: ${STARTUP_COMMAND}"
+eval ${STARTUP_COMMAND}
