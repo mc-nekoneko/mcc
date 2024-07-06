56d0b067ae8fbe48cb5e4bf7ba498df94c155eef
Nekoneko <nekonekogame@gmail.com>
2024-07-07 03:53:19 +0900

    SIGTERMシグナルを処理するよう修正

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -5,5 +5,13 @@ java -version
 
 STARTUP_COMMAND="java -Xms${SERVER_MEMORY} -Xmx${SERVER_MEMORY} ${JVM_FLAGS} -jar ${SERVER_JARFILE} ${SERVER_ARGS}"
 
+terminate() {
+  echo "Received SIGTERM, shutting down..."
+  JAVA_PID=$(ps aux | grep '[j]ava' | awk '{print $2}')
+  kill -SIGTERM "$JAVA_PID"
+}
+
+trap terminate SIGTERM
+
 echo "Running command: ${STARTUP_COMMAND}"
 eval ${STARTUP_COMMAND}
